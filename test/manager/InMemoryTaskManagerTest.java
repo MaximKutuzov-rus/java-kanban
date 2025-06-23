995f64badfe42b75c46d0f7beb1c56ff03ba7bf4
Maxim Kutuzov <kutuzovmaks2006@yandex.ru>
2025-06-23 10:34:14 +0300

    Убрать лишние import из класса тестов

--- test/manager/InMemoryTaskManagerTest.java
+++ test/manager/InMemoryTaskManagerTest.java
@@ -6,11 +6,6 @@ import org.junit.jupiter.api.Test;
 import tasks.Epic;
 import tasks.Status;
 import tasks.Subtask;
-import tasks.Task;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class InMemoryTaskManagerTest {
 
